ca0ef9ff48e253fa5d8b89cae22bfd77234e29e2
Nourenanabil <65375174+Nourenanabil@users.noreply.github.com>
2021-11-14 23:26:56 +0200

    Add files via upload

---
+++ Frontend/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+<Nav/>
+<router-view/>
+    </div>
+</template>
+
+<script>
+
+import Nav from './components/Nav.vue'
+
+export default {
+  name: 'App',
+  components: {Nav,
+}
+ 
+}
+</script>
+
+<style>
+body{
+  padding:100px;
+  
+}
+</style>
